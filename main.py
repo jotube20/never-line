3a1e5363b48e07995aaed31aa65a5028e82f2bd3
jotube20 <joujounasseif@gmail.com>
2026-02-23 22:53:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import discord
 import os
 from discord.ext import commands
+from flask import Flask
+from threading import Thread
+
+# --- سيرفر وهمي عشان Render يفضل شغال ---
+app = Flask('')
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+# ---------------------------------------
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 LINE_URL = "https://media.discordapp.net/attachments/1474909829058531335/1475499138350059600/1100196984901599343.gif"
@@ -17,19 +32,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-
-    content = message.content.lower()
-    if content == "خط" or content == "line":
+    if message.content.lower() in ["خط", "line"]:
         embed = discord.Embed(color=0xffa500)
         embed.set_image(url=LINE_URL)
         await message.channel.send(embed=embed)
-        try:
-            await message.delete()
-        except:
-            pass
+        try: await message.delete()
+        except: pass
 
-    await bot.process_commands(message)
-
-# هنا بنخلي البوت يقرأ التوكن من البيئة المحيطة (Environment Variable)
+# تشغيل السيرفر الوهمي ثم البوت
+keep_alive()
 token = os.getenv('DISCORD_TOKEN')
 bot.run(token)
